c6e0a8e78adb935adb3f934a91448ff3d3de104f
h_kang <h_kang@Ctrip.com>
2015-07-29 09:50:50 +0800

    add data model

---
+++ DataModel/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataModel
+{
+    public class Comment
+    {
+       /* 
+
+         GetTravelComments 获取某一游记关联的评论信息
+	Comments getCommentsByDiaryID()	
+        */
+        public static comment getCommentsByDiaryID(int diaryID)
+        {
+            using (var ctx = new hackthonEntities())
+            {
+                return ctx.comments.Where(x => x.diaryID == diaryID).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ DataModel/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataModel
+{
+    public class Product
+    {
+        //GetAssociatedProductsInfo  根据关键字获取关联携程产品信息
+        //String getURLByKeyword(string keyword) –返回url
+    }
+}
