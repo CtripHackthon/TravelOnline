264deed4e104d910ab612f64fd48209508289d35
zhangzhen <zhangzhen@ctrip.com>
2015-08-01 21:56:41 +0800

    add service get category

---
+++ TravelOnline/TravelService.Model/Base/Common/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelService.Model.Base.Common
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+
+        public string categoryName { get; set; }
+    }
+}
---
+++ TravelOnline/TravelService.Model/ServiceModel/GetDisplayCategoriesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelService.Model.Base.Common;
+
+namespace TravelService.Model.ServiceModel
+{
+    public class GetDisplayCategoriesResponse
+    {
+        public List<Category> categories { get; set; }
+    }
+}
