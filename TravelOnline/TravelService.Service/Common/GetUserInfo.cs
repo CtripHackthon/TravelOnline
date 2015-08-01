28b4b765785a7d45d42181a0e9eb4ae1d907f4e2
zhangzhen <zhangzhen@ctrip.com>
2015-08-01 20:36:59 +0800

    add config

--- TravelOnline/TravelService.Service/Common/GetUserInfo.cs
+++ TravelOnline/TravelService.Service/Common/GetUserInfo.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelService.Model;
+using TravelService.Model.ServiceModel;
+using TravelService.Service.Utilities;
 
 namespace TravelService.Service.Common
 {
     public class GetUserInfo : IService
     {
-        public void process(Model.ServiceRequest request, Model.ServiceResponse response)
+        public void process(ServiceRequest request, ServiceResponse response)
         {
-            throw new NotImplementedException();
+            if (request == null || request.requestObj == null)
+            {
+                response.errMessage = ReportServiceMessage.REQUEST_INVALID;
+                response.returnCode = -1;
+                return;
+            }
+
+            GetUserInfoRequest serviceRequest = (GetUserInfoRequest)request.requestObj;
+
+            if (serviceRequest.userId < 0)
+            {
+                response.errMessage = ReportServiceMessage.USER_ID_ILLEGAL;
+                response.returnCode = -1;
+                return;
+            }
+
+            GetUserInfoResponse serviceResponse = new GetUserInfoResponse();
+            response.responseObj = serviceResponse;
+            response.returnCode = 0;
+
         }
     }
 }
--- TravelOnline/TravelService.Service/Common/UserLogin.cs
+++ TravelOnline/TravelService.Service/Common/UserLogin.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataModel;
+using TravelService.Model;
+using TravelService.Model.ServiceModel;
+using TravelService.Service.Utilities;
 
 namespace TravelService.Service.Common
 {
     public class UserLogin : IService
     {
-        public void process(Model.ServiceRequest request, Model.ServiceResponse response)
+        public void process(ServiceRequest request, ServiceResponse response)
         {
-            throw new NotImplementedException();
+            if (request == null || request.requestObj == null)
+            {
+                response.errMessage = ReportServiceMessage.REQUEST_INVALID;
+                response.returnCode = -1;
+                return;
+            }
+
+            UserLoginRequest serviceRequest = (UserLoginRequest)request.requestObj;
+
+            if (serviceRequest.username == null ||
+                serviceRequest.password == null)
+            {
+                response.errMessage = ReportServiceMessage.USER_OR_PASSWORD_ILLEGAL;
+                response.returnCode = -1;
+                return;
+            }
+
+            user u = new user();
+
+
+            UserLoginResponse serviceResponse = new UserLoginResponse();
+            serviceResponse.userId = 0;
+            response.responseObj = serviceResponse;
+            response.returnCode = 0;
+
+            return;
         }
     }
 }
