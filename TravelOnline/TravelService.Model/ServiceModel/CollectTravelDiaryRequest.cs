5d8f8b7a70fe1049d7c727488f6267e6bd7281bc
zhangzhen <zhangzhen@ctrip.com>
2015-08-02 01:09:27 +0800

    add collect service

---
+++ TravelOnline/TravelService.Model/ServiceModel/CollectTravelDiaryRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelService.Model.ServiceModel
+{
+    public class CollectTravelDiaryRequest
+    {
+        public long userId { get; set; }
+
+        public long diaryId { get; set; }
+    }
+}
---
+++ TravelOnline/TravelService.Service/Travel/CollectTravelDiary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelService.Model;
+using TravelService.Model.ServiceModel;
+using TravelService.Service.Utilities;
+
+namespace TravelService.Service.Travel
+{
+    public class CollectTravelDiary : IService
+    {
+
+        public void process(ServiceRequest request, ServiceResponse response)
+        {
+            if (request == null || request.requestObj == null)
+            {
+                response.errMessage = ReportServiceMessage.REQUEST_INVALID;
+                response.returnCode = -1;
+                return;
+            }
+
+            CollectTravelDiaryRequest serviceRequest = (CollectTravelDiaryRequest)request.requestObj;
+            if (serviceRequest.diaryId < 0 || serviceRequest.userId < 0)
+            {
+                response.errMessage = ReportServiceMessage.USER_ID_OR_DIARY_ID_INVALID;
+                response.returnCode = -1;
+                return;
+            }
+            response.returnCode = 0;
+        }
+    }
+}
